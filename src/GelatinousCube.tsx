15b791ce6af49d402314cc40765eb66d4e20172f
Nick Rogers <rogers.a.nick@gmail.com>
2023-02-04 17:20:11 -0500

    Further progress on material and colors

--- src/GelatinousCube.tsx
+++ src/GelatinousCube.tsx
@@ -1,37 +1,24 @@
 import { RoundedBox } from "@react-three/drei"
-import React from "react"
+import React, { useState } from "react"
 import { colors } from "./color"
 
 const GelatinousCube = (props: { 
   playerNumber: number,
   position: [number, number, number]
 }) => {
-  const { playerNumber, position } = props
-  // const config = useControls({
-  //   scale: { value: 1, min: 0, max: 5, step: 0.01 },
-  //   transparent: false,
-  //   opacity: { value: 1, min: 0, max: 1, step: 0.01 },
-  //   samples: { value: 10, min: 1, max: 32, step: 1 },
-  //   resolution: { value: 2048, min: 256, max: 2048, step: 256 },
-  //   transmission: { value: 1, min: 0, max: 1 },
-  //   roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
-  //   ior: { value: 2, min: 1, max: 5, step: 0.01 },
-  // })
-  // config without use Controls
-  const config = {
-    scale: 1,
-    transparent: false,
-    opacity: 1,
-    samples: 10,
-    resolution: 2048,
+  const [materialConfig] = useState({
+    ior: 1.2,
+    clearcoat: 0.2,
     transmission: 1,
-    roughness: 0.0,
-    ior: 2,
-  }
+    opacity: 0.7,
+    transparent: true,
+  })
+  const [materials] = useState(colors.map(color => <meshPhysicalMaterial color={color} {...materialConfig} />))
+  const { playerNumber, position } = props
   return (
-    <group position={position} scale={config.scale}>
+    <group position={position}>
       <RoundedBox args={[1,1,1]}>
-        <meshPhysicalMaterial color={colors[playerNumber]} {...config} />
+        {materials[playerNumber]}
       </RoundedBox>
     </group>
   )
--- src/Slime.tsx
+++ src/Slime.tsx
@@ -1,15 +1,27 @@
 import { RoundedBox } from "@react-three/drei"
-import React from "react"
+import React, { useState } from "react"
+import { Color } from "three"
 import { colors } from "./color"
 
 const Slime = (props: { 
   position: [number, number, number], playerNumber: number
 }) => {
+  const [materialConfig] = useState({
+    ior: 1.2,
+    clearcoat: 0.2,
+    transmission: 0.5,
+    opacity: 1,
+    transparent: true,
+  })
+  const [materials] = useState(colors.map(color => {
+    const lightenedColor = new Color(color).lerp(new Color("white"), 0.2)
+    return <meshPhysicalMaterial color={lightenedColor} {...materialConfig} />
+  }))
   const { playerNumber, position } = props
   return (
     <group position={position}>
       <RoundedBox args={[1,0.2,1]}>
-        <meshPhysicalMaterial color={colors[playerNumber]} />
+        {materials[playerNumber]}
       </RoundedBox>
     </group>
   )
