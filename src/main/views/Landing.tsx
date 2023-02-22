3bd22a942fabedd41a1d771c46918b070ae86292
Nick Rogers <rogers.a.nick@gmail.com>
2023-02-22 13:59:49 -0500

    Updating with minor changes to landing page

--- src/main/views/Landing.tsx
+++ src/main/views/Landing.tsx
@@ -16,18 +16,22 @@ const Landing = (props: LandingProps) => {
 
   return (
     <div className={`transition-all duration-500 ease-in ${transitioning ? "translate-y-full" : ""} flex flex-col items-center h-full w-full`}>
-      <div className="container flex flex-col justify-center align-center h-1/2 mx-6 p-6">
-        <h1 className="mt-6 text-4xl font-bold text-white">ThreeCGoL</h1>
-        <h2 className="mt-6 text-2xl text-white">An implementation of Conway's Game of Life using Three.js</h2>
-        <h4 className="mt-2 text-s text-white">Also using R3F, React Spring, Web Workers, Tailwind CSS (and ChatGPT 👀)</h4>
+      <div className="container flex flex-col justify-evenly h-2/3 mx-6 px-6">
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="mt-6 text-4xl font-bold text-white">ThreeCGoL</h1>
+          <img className="w-12 h-12 rounded-xl mt-6" src="/pink_slime.png"/>
+        </div>
+        <h2 className="mt-6 text-2xl text-white">Multi-Organism Conway's Game of Life using Three.js</h2>
+        <h4 className="mt-2 text-s text-white">Stay tuned for the full game:&#13;Flourish and Decay 👀</h4>
+        <h4 className="mt-2 text-xs text-white">Also using R3F, React Spring, Web Workers, Tailwind CSS (and ChatGPT 👀)</h4>
       </div>
       <button onClick={() => {
           setTransitioning(true);
           setTimeout(() => {
             startGame();
           }, 500);
-        }} className="z-40 rounded-full bg-indigo-600 mt-12 w-24 h-24 text-xl text-white">Start</button>
-      <div className="container h-3/4 w-screen -mt-12">
+        }} className="z-40 rounded-full bg-indigo-600 mt-12 w-24 h-20 text-xl text-white">Start</button>
+      <div className="container h-3/4 w-screen -mt-16">
         <GameEngine boardSize={30} isDemo={true}>
           <GameBoard />
         </GameEngine>
