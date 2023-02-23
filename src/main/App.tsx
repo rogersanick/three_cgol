852962378a72daaa817db977e3e0b678a5f31cca
Nick Rogers <rogers.a.nick@gmail.com>
2023-02-23 13:41:53 -0500

    Adjust background gradient

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -8,12 +8,11 @@ const App = () => {
   const Landing = lazy(() => import('./views/Landing'));
   const CgolGame = lazy(() => import('./views/CgolGame'));
   return (
-      <div className="animate-gradient-xy bg-gradient-to-tr from-purple-900 via-slate-900 to-purple-900 h-screen w-screen z-30 overflow-hidden">
+      <div className="animate-gradient-xy bg-gradient-to-br from-blue-800 via-slate-900 to-indigo-700 h-screen w-screen z-30 overflow-hidden">
       <Suspense fallback={<LoadingIndicator/>}>
-        { !cgolGameStarted && <video autoPlay muted loop className="opacity-20 z-0 w-full h-full absolute top-0 left-0 object-cover">
-            <source src="/app_background.mp4" type="video/mp4" />
-          </video>
-        }
+        <video autoPlay muted loop className="opacity-20 z-0 w-full h-full absolute top-0 left-0 object-cover">
+          <source src="/app_background.mp4" type="video/mp4" />
+        </video>
         { cgolGameStarted ?
         <CgolGame /> :
         <Landing startGame={() => setCgolGameStarted(true)} /> }
